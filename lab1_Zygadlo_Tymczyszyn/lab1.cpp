830b3696d72358d640d0dc4fbe6cf22ebd7c8d3d
Zet <264193@student.pwr.edu.pl>
2024-03-13 11:35:19 +0100

    Dodano nową metodę sortowania i zastosowano ją w funkcji main()

---
+++ lab1_Zygadlo_Tymczyszyn/lab1.cpp
@@ -0,0 +1,14 @@
+# include "klasa.cpp"
+
+
+int main(){
+
+    string danetxt = "/Users/zet/Studia/KZW/lab1_Zygadlo_Tymczyszyn/";
+    
+    wszystkieRozwiazania dane(danetxt,4);
+    dane.rozwiazWszystkie(InMiddleLongestR);
+
+
+
+    return 0;
+}
